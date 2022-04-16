45566d5f372cf15e835fbf26463d6eadd0bafff5
Janne Taskinen <janne.taskinen@tuni.fi>
2022-04-16 21:32:50 +0300

    Mergesort fixed

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -8,12 +8,13 @@ def debug_print(debug_msg=None, **kwargs):
         print("{}: {}".format(key, value))
 
 
-<<<<<<< HEAD
 def mergesort(array):
+    debug_print(array=array)
     if len(array) <= 1:
         return array
 
     m = len(array) // 2
+    debug_print(m=m)
 
     left = mergesort(array[:m])
     right = mergesort(array[m:])
@@ -22,6 +23,8 @@ def mergesort(array):
 
 
 def merge(left, right):
+    debug_print(debug_msg="Merging...", left=left, right=right)
+
     merged = []
 
     while len(left) > 0 and len(right) > 0:
@@ -35,14 +38,16 @@ def merge(left, right):
     else:
         merged += right
 
+    debug_print(merged=merged)
     return merged
 
 
-=======
->>>>>>> parent of dd8c56c... Merge-sort
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
+
     input_list = input_str.split(",")
+    debug_print(input_list=input_list)
+
     value_list = []
     for x in input_list:
         try:
@@ -51,9 +56,7 @@ if __name__ == "__main__":
             print("Invalid input.")
             quit(1)
 
-<<<<<<< HEAD
+    debug_print(value_list=value_list)
+
     sorted_list = mergesort(value_list)
     print(sorted_list)
-=======
-    debug_print(value_list=value_list)
->>>>>>> parent of dd8c56c... Merge-sort
